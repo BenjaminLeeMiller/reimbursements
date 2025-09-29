460c87a9fbfa2f5d3a8f90881b115a9c873244f9
BenjaminLeeMiller <benjamin.lee.miller@gmail.com>
2025-09-29 17:14:44 -0400

    add Project class and specs

---
+++ spec/lib/project_spec.rb
@@ -0,0 +1,51 @@
+require 'project'
+
+RSpec.describe Project do
+  describe "#new" do
+    let(:start_date) { Date.new }
+    let(:end_date) { Date.new }
+    let(:scale) { PayScales::LOW }
+
+    subject { described_class.new(start_date, end_date, scale) }
+
+    context "with valid params" do
+      it 'should return a new instance.' do
+        expect(subject).to be_a described_class
+      end
+    end
+
+    context "when the start date is" do
+      context "not a date" do
+        let(:start_date) { "Not a Date" }
+
+        it 'should raise InvalidStartDate' do
+          expect { subject }.to raise_error(Project::InvalidStartDate)
+        end
+      end
+
+      context "after the end date" do
+        let(:start_date) { Date.new.next_day }
+
+        it 'should raise InvalidDateRange' do
+          expect { subject }.to raise_error(Project::InvalidDateRange)
+        end
+      end
+    end
+
+    context "when the end date is not a date" do
+      let(:end_date) { "Not a Date" }
+
+      it 'should raise InvalidEndDate' do
+        expect { subject }.to raise_error(Project::InvalidEndDate)
+      end
+    end
+
+    context "when the scale is invalid" do
+      let(:scale) { "bob" }
+
+      it 'should raise InvalidScale' do
+        expect { subject }.to raise_error(Project::InvalidScale)
+      end
+    end
+  end
+end
